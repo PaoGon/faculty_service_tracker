5ed6447806959d28951c1cf618c9906e34764d80
gitmccoyabrasaldo <mccoyabrasaldo@gmail.com>
2022-05-26 09:39:03 +0800

    First commit- Create landing_page(), landing_page2() and login_page they are all redirect in next page.

--- app/src/main/java/com/example/faculty_service_tracker/login_page.java
+++ app/src/main/java/com/example/faculty_service_tracker/login_page.java
@@ -2,35 +2,26 @@ package com.example.faculty_service_tracker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class login_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_page);
 
-        /*Landing Page2 button*/
-        Button login_btn =findViewById(R.id.login_btn);
-        login_btn.setOnClickListener(view -> landing_page2());
-
+        /*onclick back*/
+        ImageView ic_back = findViewById(R.id.ic_back);
+        ic_back.setOnClickListener(view -> landing_page2());
 
 
     }
-
-
-
-    /*Login button for landing_page2*/
-    private void landing_page2() {
+        /*button back to landing page2*/
+    private void landing_page2(){
         Intent intent = new Intent(this, landing_page2.class);
         startActivity(intent);
     }
-
-
-
-
 }
