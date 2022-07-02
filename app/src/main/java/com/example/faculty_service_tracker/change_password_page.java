f0cbf83c0638a51ce52510c5074ada4c3afa8e2a
micmic6 <micahguinto@gmail.com>
2022-07-02 10:24:55 +0800

    added activity change password page and activity edit account page

---
+++ app/src/main/java/com/example/faculty_service_tracker/change_password_page.java
@@ -0,0 +1,14 @@
+package com.example.faculty_service_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class change_password_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password_page);
+    }
+}
